2d182f5029e1468f80af7ddd8af7e867bab07258
Jeremy Ottley <Gazaunga@gmail.com>
2019-07-08 08:51:08 -0400

    Update clock.rb

--- clock.rb
+++ clock.rb
@@ -4,3 +4,15 @@ starting = Process.clock_gettime(Process::CLOCK_MONOTONIC)
 ending = Process.clock_gettime(Process::CLOCK_MONOTONIC)
 elapsed = ((ending - starting) * 100).round / 100.0 # round long float down to hundreths place
 elapsed # => time in seconds
+
+###
+
+module Clock
+  def self.time
+    starting = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+    yield if block_given?
+    ending = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+    elapsed = ((ending - starting) * 100).round / 100.0
+    elapsed
+  end
+end #=> Clock.time { sleep(2.545) }
