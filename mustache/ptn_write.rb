ca1963bf307e77485e96d4ccf1b38971cd6974ac
jeremyottley <jeremy.ottley@gmail.com>
2019-07-09 10:16:49 -0400

    continued

--- mustache/ptn_write.rb
+++ mustache/ptn_write.rb
@@ -1,6 +1,6 @@
-require 'mustache'
+require "mustache"
 
-Mustache.template_file = File.dirname(__FILE__) + '/ptn.mustache'
+Mustache.template_file = File.dirname(__FILE__) + "/ptn_header.mustache"
 
 ptn = Mustache.new
 
@@ -14,8 +14,25 @@ ptn[:Clock] = "10:0 +20"
 ptn[:Result] = "R-0"
 ptn[:Size] = "5"
 
-
 File.open("match.ptn", "w") do |file|
   file.puts(ptn.render)
   file.puts("\n")
+end
+
+##
+
+File.open("match.ptn", "a+") do |file|
+ move_hash = {
+  1 => "a6 f6",
+  2 => "d4 c4",
+  3 => "d3 c3",
+  4 => "d5 c5",
+  5 => "d2 Ce4",
+  6 => "c2 e3",
+  7 => "e2 b2",
+  8 => "Cb3 1e4<1",
+  9 => "1d3<1 Sd1",
+  10 => "a3 1d1+1",
+}
+  move_hash.each {|k,v| file.puts "#{k}. #{v}" + "\n"}
 end
